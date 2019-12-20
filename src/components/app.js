898f3605a5da7e17c15aa806c604814a7edd5378
Raymond Martinez <rcmartinez1854@gmail.com>
2019-12-20 10:20:05 -0800

    added router

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AboutMenu from './about-menu';
+import { Route, Switch } from 'react-router-dom';
 
-export default function App(props) {
-    return (
-        <div className="app">
-            {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
-        </div>
-    );
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
+                <Switch>
+                    <Route exact path='/' component={AboutMenu} />
+                    <Route exact path='about/foo' component={AboutMenu} />
+                    <Route exact path='about/bar' component={AboutMenu} />
+                    <Route exact path='about/foo/bar' component={AboutMenu} />  
+                </Switch>    
+                {/* <AboutMenu /> */}
+            </div>
+        );
+    }
 }
